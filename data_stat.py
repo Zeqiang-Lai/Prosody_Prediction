f2136ff92be0a54fbe5eb69b9b1538a72287bdf9
lzq <laizeqiang@kuaishou.com>
2019-08-06 11:30:12 +0800

    experiments: train biaobei1

--- data_stat.py
+++ data_stat.py
@@ -1,5 +1,5 @@
 if __name__ == '__main__':
-    file_path = 'data/biaobei3/train/labels.txt'
+    file_path = 'data/biaobei1/val/labels.txt'
     with open(file_path, 'r') as f:
         lines = f.readlines()
         lines = [line.strip() for line in lines]
@@ -13,5 +13,5 @@ if __name__ == '__main__':
         I = items.count('I')
         print("B: {0}".format(B))
         print("I: {0}".format(I))
-        print(I/(B+I))
+        print(B/(B+I))
         print((I+len(lines)) / (B+I))
