8dd27a063bb2a5a4b9869842b007cb2af8eea440
laizeqiang <laizeqiang@kuaishou.com>
2020-02-19 16:53:50 +0800

    Update download URL

--- embedding/download_biaobei_embedding.sh
+++ embedding/download_biaobei_embedding.sh
@@ -1,4 +1,4 @@
 BASEDIR=$(dirname "$0")
-wget http://dvmvd-4602.kmlltpro.corp.kuaishou.com/prosody_prediction_biaobei_embedding.tar.gz -O "$BASEDIR"/prosody_prediction_biaobei_embedding.tar.gz
-tar -zxvf "$BASEDIR"/prosody_prediction_biaobei_embedding.tar.gz -C "$BASEDIR"
-rm "$BASEDIR"/prosody_prediction_biaobei_embedding.tar.gz
+wget http://dvmvd-4602.kmlltpro.corp.kuaishou.com/prosody_prediction/biaobei_embedding.tar.gz -O "$BASEDIR"/biaobei_embedding.tar.gz
+tar -zxvf "$BASEDIR"/biaobei_embedding.tar.gz -C "$BASEDIR"
+rm "$BASEDIR"/biaobei_embedding.tar.gz
--- pretrained/download_biaobei_pretrained.sh
+++ pretrained/download_biaobei_pretrained.sh
@@ -1,4 +1,4 @@
 BASEDIR=$(dirname "$0")
-wget http://dvmvd-4602.kmlltpro.corp.kuaishou.com/prosody_prediction_biaobei_pretrained.tar.gz -O "$BASEDIR"/prosody_prediction_biaobei_pretrained.tar.gz
-tar -zxvf "$BASEDIR"/prosody_prediction_biaobei_pretrained.tar.gz -C "$BASEDIR"
-rm "$BASEDIR"/prosody_prediction_biaobei_pretrained.tar.gz
+wget http://dvmvd-4602.kmlltpro.corp.kuaishou.com/prosody_prediction/biaobei_pretrained.tar.gz -O "$BASEDIR"/biaobei_pretrained.tar.gz
+tar -zxvf "$BASEDIR"/biaobei_pretrained.tar.gz -C "$BASEDIR"
+rm "$BASEDIR"/biaobei_pretrained.tar.gz
