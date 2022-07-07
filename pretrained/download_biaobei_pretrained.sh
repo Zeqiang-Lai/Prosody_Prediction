748090bc7c882e8dc991ec3fac18db9b43ad6db4
Zeqiang Lai <916323301@qq.com>
2022-07-07 15:40:55 +0800

    Delete download_biaobei_pretrained.sh

--- pretrained/download_biaobei_pretrained.sh
+++
@@ -1,4 +0,0 @@
-BASEDIR=$(dirname "$0")
-wget http://dvmvd-4602.kmlltpro.corp.kuaishou.com/prosody_prediction/biaobei_pretrained.tar.gz -O "$BASEDIR"/biaobei_pretrained.tar.gz
-tar -zxvf "$BASEDIR"/biaobei_pretrained.tar.gz -C "$BASEDIR"
-rm "$BASEDIR"/biaobei_pretrained.tar.gz
