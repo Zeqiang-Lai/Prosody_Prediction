3e882b0bc9afdb90944ad4cab9ef35157b331e1d
Zeqiang Lai <916323301@qq.com>
2022-07-07 15:42:11 +0800

    Delete download_biaobei_embedding.sh

--- embedding/download_biaobei_embedding.sh
+++
@@ -1,4 +0,0 @@
-BASEDIR=$(dirname "$0")
-wget http://dvmvd-4602.kmlltpro.corp.kuaishou.com/prosody_prediction/biaobei_embedding.tar.gz -O "$BASEDIR"/biaobei_embedding.tar.gz
-tar -zxvf "$BASEDIR"/biaobei_embedding.tar.gz -C "$BASEDIR"
-rm "$BASEDIR"/biaobei_embedding.tar.gz
